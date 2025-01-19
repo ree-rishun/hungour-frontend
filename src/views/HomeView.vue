6219dcd5f2f965615fd6fd7bd170447f1ac91c86
ree-rishun <ree0432@gmail.com>
2025-01-19 16:42:06 +0900

    feature: #2 現在地の取得

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,64 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
+  import {
+    onMounted,
+    ref,
+  } from 'vue'
+
+  const location = ref({
+    lat: null,
+    lng: null,
+  })
+  const errMessage = ref('')
+
+  onMounted(
+    async () => {
+      // 現在地の取得
+      const position = await getLocation()
+      location.value.lat = position.coords.latitude
+      location.value.lng = position.coords.longitude
+      console.log(location.value)
+
+      // 飲食店一覧の検索
+    }
+  )
+
+  // 現在地の取得
+  const getLocation = () => {
+    return new Promise((resolve, reject) => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          position => resolve(position),
+          err => {
+            errMessage.value = '不明なエラーが発生しました'
+            if (err.code === err.PERMISSION_DENIED) {
+              errMessage.value = '位置情報の取得が許可されていません'
+            }
+            if (err.code === err.POSITION_UNAVAILABLE) {
+              errMessage.value = '位置情報を取得できませんでした'
+            }
+            if (err.code === err.TIMEOUT) {
+              errMessage.value = '位置情報の取得にタイムアウトしました'
+            }
+          }
+        )
+      } else {
+        errMessage.value = 'Geolocation API がサポートされていません'
+      }
+    })
+  }
+
+  // 飲食店一覧の検索
+  const getPlaces = async () => {
+
+  }
 </script>
 
-<template>
-</template>
 
 <style lang="scss" scoped>
 </style>
