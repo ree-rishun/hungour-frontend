d30f1ec1c072621516ff9f30f358de727f8369fd
ree-rishun <ree0432@gmail.com>
2025-02-06 07:56:56 +0900

    feature: #6 タイトルコンポーネントの作成

---
+++ src/components/shared/title.component.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>
+    {{ props.title }}
+  </h2>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true,
+    }
+  })
+
+</script>
+
+<style scoped lang="scss">
+  h2 {
+    margin: 16px auto 0;
+    font-weight: bold;
+    font-size: 20px;
+    color: #ffffff;
+  }
+</style>
